09d8fbc3c0d3454535018c1be4864a4c65ee34be
andyk1278 <andyk1278@gmail.com>
2017-02-24 15:33:37 -0800

    added ModelSerializer for Chain, Store, and Employee

--- server/retail/models.py
+++ server/retail/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class Chain(models.Model):
     """ retail chain model"""
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    slogan = models.CharField(max_length=1000)
+    slogan = models.CharField(max_length=500)
     founded_date = models.CharField(max_length=500)
     website = models.URLField(max_length=500)
 
@@ -17,7 +18,7 @@ class Store(models.Model):
     number = models.CharField(max_length=20)
     address = models.CharField(max_length=1000)
     opening_date = models.DateTimeField(default=timezone.now)
-    #Business hours in a 24 hour clock. Deafult 8am-5pm
+    # Business hours in a 24 hour clock. Deafult 8am-5pm
     business_hours_start = models.IntegerField(
         default=8,
         validators=[
@@ -25,6 +26,14 @@ class Store(models.Model):
             MaxValueValidator(23)
         ]
     )
+    business_hours_end = models.IntegerField(
+        default=17,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(23)
+        ]
+    )
+
 
 class Employee(models.Model):
     """ Employee model. Foreign key to its Store"""
