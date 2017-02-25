4ee8b3decde156c09b164942c4660c8af33e3b40
andyk1278 <andyk1278@gmail.com>
2017-02-24 16:04:04 -0800

    defined all retail views and defined url routes

--- server/urls.py
+++ server/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+from rest_framework.routers import DefaultRouter
+from retail.views import ChainViewSet, StoreViewSet, EmployeeViewSet
+
+router = DefaultRouter()
+router.register(prefix='chains', viewset=ChainViewSet)
+router.register(prefix='stores', viewset=StoreViewSet)
+router.register(prefix='employees', viewset=EmployeeViewSet)
+
+urlpatterns = router.urls
+#urlpatterns = [
+#    url(r'^admin/', include(admin.site.urls)),
+#]
